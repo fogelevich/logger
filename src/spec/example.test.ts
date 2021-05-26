aad6b5dd81051f96c25776019e9facbeb2e74455
Eugene Tealon <admin@biptik.com>
2021-05-26 10:40:18 +0300

    Fix test logger (#1)
    * test
    
    * fix test
    
    * add to gitignore rescript
    
    * ignore types cache
    
    Co-authored-by: fogelevich <fogelevich@gmail.com>

--- src/spec/example.test.ts
+++ src/spec/example.test.ts
@@ -1,7 +1,25 @@
 import { ConsoleLogger } from "../logger";
 
-const log = new ConsoleLogger("Example Log", "WARN");
+function testPriority(name) {
+  console.log("-------------------------------");
+  console.log(`Priority - ${name}`);
+  console.log("-------------------------------");
+}
 
+let log = new ConsoleLogger("Example Log");
+
+testPriority("DEFAULT");
+log.error("test error");
+log.info("test info");
+log.verbose("test verbose");
+log.warn("test warn");
+log.log("test log");
+log.debug("test debug");
+log.trace("test trace");
+
+log = new ConsoleLogger("Example Log", "ERROR");
+
+testPriority("ERROR");
 log.error("test error");
 log.info("test info");
 log.verbose("test verbose");
